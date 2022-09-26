dcd55748a00fdf8807f9a75be3e8a15d34279f1c
Troneci Andrei <troneciandrei@gmail.com>
2022-09-26 20:27:13 +0300

    Pixellab-ecom

---
+++ pages/contact.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const ContactPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact Page</title>
+      </Head>
+
+      <main>hello from next</main>
+    </>
+  );
+};
+
+export default ContactPage;
