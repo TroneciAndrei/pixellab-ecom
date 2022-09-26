88c199ba4f9c0064807614c6b83abb64b3e3afab
Troneci Andrei <troneciandrei@gmail.com>
2022-09-26 21:42:08 +0300

    Ecom setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main className="text-3xl underline bg-red-50">Homepage</main>
+    </>
+  );
+};
+
+export default Home;
