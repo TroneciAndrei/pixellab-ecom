c8f400f194d2d06e22e080da01ee090ad6992e1a
Troneci Andrei <troneciandrei@gmail.com>
2022-10-24 22:03:25 +0300

    Curs 24.10

--- src/components/catalog/GridControls.jsx
+++ src/components/catalog/GridControls.jsx
@@ -5,13 +5,15 @@ import {
   BsFillGridFill,
   BsFillGrid3X3GapFill,
 } from "react-icons/bs";
+import { useIsMobile, useLocalStorage } from "../../hooks";
 
 const buttonClasses = `
     w-24 h-24  border-l border-zinc-400 border-r-0 flex justify-center items-center
 `;
 
 export const GridControls = ({ setPerRow = () => {} }) => {
-  const [itemsPerRow, setItemsPerRow] = useState("4/row");
+  const [itemsPerRow, setItemsPerRow] = useLocalStorage("perRow", "4/row");
+  const isMobile = useIsMobile();
 
   // de fiecare data cand itemsPerRow se schimba ruleaza acest callback
   useEffect(() => {
@@ -19,6 +21,10 @@ export const GridControls = ({ setPerRow = () => {} }) => {
 
     setPerRow(perRow);
   }, [itemsPerRow, setPerRow]);
+
+  if (isMobile === true) {
+    return <></>;
+  }
 
   return (
     <ul className="flex border border-l-0 border-zinc-400 ">
